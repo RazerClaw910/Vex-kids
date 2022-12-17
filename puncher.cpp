93870c28fef05571887c8e59741858975a8bd980
Bobcrobisjob <76071718+Bobcrobisjob@users.noreply.github.com>
2022-12-17 08:32:46 -0500

    Update puncher.cpp

--- puncher.cpp
+++ puncher.cpp
@@ -114,6 +114,19 @@ void usercontrol(void) {
     else if (Controller1.ButtonL2.pressing()) {intake.spin(reverse, 12, volt);}
     else if (Controller1.ButtonL1.pressing()) {intake.spin(forward, 12, volt);}
     else {intake.stop();}
+   
+    Vision14.takeSnapshot(Vision14__SIG_1);  //Take a picture
+    //If it didn't see anything, take a picture looking for something different.
+    if (!Vision14.largestObject.exists) {Vision14.takeSnapshot(Vision14__SIG_2);} 
+    
+    //Pid TIMEEE
+    //Sensor in is Vision.largestobject.centerX, which returns the horizontal center
+    err=165-Vision14.largestObject.centerX;  //165 is my desired value.  
+    speed=err-lasterr;
+    lasterr=err;
+    pidout=err*.08+speed*.12;  //I directly set my kp and kd without variables.
+
+    if (!Controller1.ButtonX.pressing()||!Vision14.largestObject.exists) {pidout=0;}
 
     drive=Controller1.Axis3.position()*.12;
     turn=Controller1.Axis1.position()*.12*.45; //- pidout;//add pidout
